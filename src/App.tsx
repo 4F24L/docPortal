fea7ad055c6ea9a3da8b8d88f1d9d45497dd3e3f
4F24L <afzalmir2003@gmail.com>
2025-05-27 17:21:54 +0530

    deploy fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,16 @@ import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import NotFound from "./pages/NotFound";
 
+const router = {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true
+  }
+};
+
 const App = () => (
   <AuthProvider>
-    <BrowserRouter>
+    <BrowserRouter future={router.future}>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/signup" element={<Signup />} />
